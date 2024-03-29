3b2832ce8ff2a4d02566ebc4f09fdd643683ba50
str79 <lcas3900@gmail.com>
2024-03-29 20:29:53 +0300

    update
    update

--- lang/en/settings.js
+++ lang/en/settings.js
@@ -7,7 +7,67 @@ var Profiles=[
 		'StartIndex':1,
 		'offsetLeft':'626px',
 		'offsetTop':'265px',
-		'GpoupList':['Полезное'],
+		'GpoupList':['Useful'],
+	},
+	{
+		'Name':'map8',
+		'File':'img/map8.jpg',
+		'pointarr':'map8Pt',
+		'zoom':1,
+		'StartIndex':1,
+		'offsetLeft':'-16px',
+		'offsetTop':'-44px',
+		'GpoupList':['Useful','Miscellaneous'],
+	},
+	{
+		'Name':'map7',
+		'File':'img/map7.jpg',
+		'pointarr':'map7Pt',
+		'zoom':1.1,
+		'StartIndex':1,
+		'offsetLeft':'224px',
+		'offsetTop':'-58px',
+		'GpoupList':['Useful','Miscellaneous'],
+	},
+	{
+		'Name':'map4',
+		'File':'img/map4.jpg',
+		'pointarr':'map4Pt',
+		'zoom':2,
+		'StartIndex':1,
+		'offsetLeft':'626px',
+		'offsetTop':'265px',
+		'GpoupList':['Useful','Miscellaneous'],
+	},
+	{
+		'Name':'map5',
+		'File':'img/map5.jpg',
+		'pointarr':'map5Pt',
+		'zoom':2,
+		'StartIndex':1,
+		'offsetLeft':'626px',
+		'offsetTop':'265px',
+		'GpoupList':['Useful'],
+	},
+	{
+		'Name':'map6',
+		'File':'img/map6.jpg',
+		'pointarr':'map6Pt',
+		'zoom':0.9,
+		'StartIndex':1,
+		'offsetLeft':'-60px',
+		'offsetTop':'-10px',
+		'GpoupList':['Useful'],
+	},
+	{
+		'Name':'map3',
+		'File':'img/map3.jpg',
+		'pointarr':'map3Pt',
+		'zoom':2,
+		'StartIndex':1,
+		'offsetLeft':'626px',
+		'offsetTop':'265px',
+		'GpoupList':['Useful'],
 	},
 	{
 		'Name':'map2',
@@ -17,29 +77,149 @@ var Profiles=[
 		'StartIndex':1,
 		'offsetLeft':'626px',
 		'offsetTop':'265px',
-		'GpoupList':['Полезное'],
+		'GpoupList':['Useful'],
+		},
+]
+
+var map8Pt=[
+	{
+		'Name' : 'Kangaroo (in a circle).',
+		'CoordX' : '642.217px',
+		'CoordY' : '557.783px',
+		'Groups' : '[0]',
 	},
 ];
-
+var map7Pt=[
+	{
+		'Name' : 'Kangaroo, first boutique from the entrance, yellow round barrel.',
+		'CoordX' : '594px',
+		'CoordY' : '496px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'The roof is full of upgrade slots',
+		'CoordX' : '627px',
+		'CoordY' : '498px',
+		'Groups' : '[1]',
+	},
+];
+var map4Pt=[
+	{
+		'Name' : 'Pedestrians on the map, in the pipe.',
+		'CoordX' : '340px',
+		'CoordY' : '205px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Pedestrians on the map, on the roofs of houses.',
+		'CoordX' : '154px',
+		'CoordY' : '76px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Pedestrians on the map in the middle of the road.',
+		'CoordX' : '521px',
+		'CoordY' : '195px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Rooftop ride',
+		'CoordX' : '534px',
+		'CoordY' : '189px',
+		'Groups' : '[1]',
+	},
+];
+var map5Pt=[
+	{
+		'Name' : 'Pedestrians on the map (between houses).',
+		'CoordX' : '236px',
+		'CoordY' : '218px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Pedestrians on the map (to the right of the road).',
+		'CoordX' : '212px',
+		'CoordY' : '113px',
+		'Groups' : '[0]',
+	},
+];
+var map6Pt=[
+	{
+		'Name' : 'Pedestrians on the map (underwater).',
+		'CoordX' : '1279.33px',
+		'CoordY' : '573.667px',
+		'Groups' : '[0]',
+	},
+];
+var map3Pt=[
+	{
+		'Name' : 'Pedestrians on the map.',
+		'CoordX' : '36px',
+		'CoordY' : '51px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Armor Slots',
+		'CoordX' : '530px',
+		'CoordY' : '242px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Armor',
+		'CoordX' : '174px',
+		'CoordY' : '309px',
+		'Groups' : '[0]',
+	},
+];
 var map2Pt=[
 	{
-		'Name' : 'New',
-		'CoordX' : '0',
-		'CoordY' : '0',
+		'Name' : 'Pedestrian repulsifiers',
+		'CoordX' : '288px',
+		'CoordY' : '273.5px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Fills all slots',
+		'CoordX' : '317.5px',
+		'CoordY' : '222px',
 		'Groups' : '[0]',
 	},
 ];
 var map1Pt=[
 	{
-		'Name':'Броня в мойке',
+		'Name':'Armor in the sink',
 		'CoordX':'130px',
 		'CoordY':'294px',
 		'Groups':'[0]',
 	},
 	{
-		'Name':'Пешеходы на карте где-то тут.',
-		'CoordX':'475px',
-		'CoordY':'134px',
+		'Name':'Pedestrians on the map.',
+		'CoordX':'482px',
+		'CoordY':'132px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Roof Engine',
+		'CoordX':'189px',
+		'CoordY':'131px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Fist (strength) on the roof.',
+		'CoordX':'223px',
+		'CoordY':'259px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Pedestrians on the map.',
+		'CoordX':'310px',
+		'CoordY':'220px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Solid granite machine.',
+		'CoordX':'136px',
+		'CoordY':'274px',
 		'Groups':'[0]',
 	},
 ];
--- lang/ru/settings.js
+++ lang/ru/settings.js
@@ -1,5 +1,35 @@
 var Profiles=[
 	{
+		'Name':'map1',
+		'File':'img/map1.jpg',
+		'pointarr':'map1Pt',
+		'zoom':2,
+		'StartIndex':1,
+		'offsetLeft':'626px',
+		'offsetTop':'265px',
+		'GpoupList':['Полезное'],
+	},
+	{
+		'Name':'map8',
+		'File':'img/map8.jpg',
+		'pointarr':'map8Pt',
+		'zoom':1,
+		'StartIndex':1,
+		'offsetLeft':'-16px',
+		'offsetTop':'-44px',
+		'GpoupList':['Полезное','Разное'],
+	},
+	{
+		'Name':'map7',
+		'File':'img/map7.jpg',
+		'pointarr':'map7Pt',
+		'zoom':1.1,
+		'StartIndex':1,
+		'offsetLeft':'224px',
+		'offsetTop':'-58px',
+		'GpoupList':['Полезное','Разное'],
+	},
+	{
 		'Name':'map4',
 		'File':'img/map4.jpg',
 		'pointarr':'map4Pt',
@@ -7,7 +37,7 @@ var Profiles=[
 		'StartIndex':1,
 		'offsetLeft':'626px',
 		'offsetTop':'265px',
-		'GpoupList':['Полезное'],
+		'GpoupList':['Полезное','Разное'],
 	},
 	{
 		'Name':'map5',
@@ -49,18 +79,56 @@ var Profiles=[
 		'offsetTop':'265px',
 		'GpoupList':['Полезное'],
 	},
-	{
-		'Name':'map1',
-		'File':'img/map1.jpg',
-		'pointarr':'map1Pt',
-		'zoom':2,
-		'StartIndex':1,
-		'offsetLeft':'626px',
-		'offsetTop':'265px',
-		'GpoupList':['Полезное'],
-	},
 ]
 
+var map8Pt=[
+	{
+		'Name' : 'Кенгуру (в круге).',
+		'CoordX' : '642.217px',
+		'CoordY' : '557.783px',
+		'Groups' : '[0]',
+	},
+];
+var map7Pt=[
+	{
+		'Name' : 'Кенгуру, первый бутик от входа, желтая круглая бочка.',
+		'CoordX' : '594px',
+		'CoordY' : '496px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'На крыше полно слотов для улучшений',
+		'CoordX' : '627px',
+		'CoordY' : '498px',
+		'Groups' : '[1]',
+	},
+];
+var map4Pt=[
+	{
+		'Name' : 'Пешеходы на карте, в трубе.',
+		'CoordX' : '340px',
+		'CoordY' : '205px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Пешеходы на карте, на крыше домов.',
+		'CoordX' : '154px',
+		'CoordY' : '76px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Пешеходы на карте посреди дороги.',
+		'CoordX' : '521px',
+		'CoordY' : '195px',
+		'Groups' : '[0]',
+	},
+	{
+		'Name' : 'Заезд на крышу',
+		'CoordX' : '534px',
+		'CoordY' : '189px',
+		'Groups' : '[1]',
+	},
+];
 var map5Pt=[
 	{
 		'Name' : 'Пешеходы на карте (между домами).',
@@ -119,41 +187,39 @@ var map2Pt=[
 ];
 var map1Pt=[
 	{
-		'Name' : 'Броня в мойке',
-		'CoordX' : '130px',
-		'CoordY' : '294px',
-		'Groups' : '[0]',
-	},
-	{
-		'Name' : 'Пешеходы на карте.',
-		'CoordX' : '482px',
-		'CoordY' : '132px',
-		'Groups' : '[0]',
-	},
-	{
-		'Name' : 'Двигатель на крыше',
-		'CoordX' : '189px',
-		'CoordY' : '131px',
-		'Groups' : '[0]',
-	},
-	{
-		'Name' : 'Кулак (сила) на крыше.',
-		'CoordX' : '223px',
-		'CoordY' : '259px',
-		'Groups' : '[0]',
-	},
-];
-var map4Pt=[
-	{
-		'Name':'Пешеходы на карте, в трубе.',
-		'CoordX':'340px',
-		'CoordY':'205px',
+		'Name':'Броня в мойке',
+		'CoordX':'130px',
+		'CoordY':'294px',
 		'Groups':'[0]',
 	},
 	{
-		'Name':'Пешеходы на карте, на крыше домов.',
-		'CoordX':'154px',
-		'CoordY':'76px',
+		'Name':'Пешеходы на карте.',
+		'CoordX':'482px',
+		'CoordY':'132px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Двигатель на крыше',
+		'CoordX':'189px',
+		'CoordY':'131px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Кулак (сила) на крыше.',
+		'CoordX':'223px',
+		'CoordY':'259px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Пешеходы на карте.',
+		'CoordX':'310px',
+		'CoordY':'220px',
+		'Groups':'[0]',
+	},
+	{
+		'Name':'Солидная гранитная машина.',
+		'CoordX':'136px',
+		'CoordY':'274px',
 		'Groups':'[0]',
 	},
 ];
